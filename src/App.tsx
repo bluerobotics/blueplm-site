5223bb84fc6ccdae6665d7320c2a579d539ef313
Eldin Miller-Stead <49656283+eldinmiller@users.noreply.github.com>
2025-12-30 23:41:40 -0800

    Add PNG favicons and update structured data for Google Search logo

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Layout from './components/Layout'
 import Home from './pages/Home'
 import Downloads from './pages/Downloads'
 import Donate from './pages/Donate'
+import Privacy from './pages/Privacy'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route index element={<Home />} />
         <Route path="downloads" element={<Downloads />} />
         <Route path="donate" element={<Donate />} />
+        <Route path="privacy" element={<Privacy />} />
       </Route>
     </Routes>
   )
